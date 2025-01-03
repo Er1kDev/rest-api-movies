1960bf2caf609cdb2a22a4935fc00b13c301f74d
Er1kDev <erick_caceres@outlook.cl>
2025-01-03 10:09:33 -0300

    arreglo de un error de cors

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(cors(
         origin: (origin, callback) => {
             const ACCEPTED_ORIGINS = ['http://localhost:8080', 'http://localhost:1234'];
             if (ACCEPTED_ORIGINS.includes(origin)) {
-                callback(null, true);
+                return callback(null, true);
             }
             if (!origin) {
-                callback(null, true);
+                return callback(null, true);
             }
 
             return callback(new Error('Not allowed by CORS'));
